cbc743228a4afaa08a5fadc330aee36a2d2598ba
Nicolle-12 <vitor.bracinho15@gmail.com>
2024-11-25 11:26:24 -0300

    Add files via upload

---
+++ pergunta.js
@@ -0,0 +1,19 @@
+criaCartao(
+    'Geografia',
+    'Quais os tipos de rochas?',
+    'sedimentares, metamórficas e magmaticas'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais os tipos modais?',
+    'Rodoviário, ferroviário, aquaviário, aeroviário e dutoviário .'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais os benefícios do modal rodoviário?',
+    'Ele permite melhor flexibilidade, além de facilitar a interação entre as regiões.'
+)
+
+
